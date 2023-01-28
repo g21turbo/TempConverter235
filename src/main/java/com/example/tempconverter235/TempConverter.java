dc61d993c98a0fc21aa32c3a8dc6f211585fce99
User <dg21stew@gmail.com>
2023-01-27 16:02:35 -0800

    added 'To' label

--- src/main/java/com/example/tempconverter235/TempConverter.java
+++ src/main/java/com/example/tempconverter235/TempConverter.java
@@ -3,6 +3,8 @@ package com.example.tempconverter235;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +12,8 @@ import java.io.IOException;
 public class TempConverter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Image image = new Image("/Images/thermoicon.jpeg");
+       // ImageView imageView = new ImageView(image);
         FXMLLoader fxmlLoader = new FXMLLoader(TempConverter.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 333);
         stage.setTitle("Temperature Converter");
